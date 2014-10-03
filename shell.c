b39c727a08b9992852b6835d0e435107d7bc3f9f
Jsimmons--dev <simmons.j@me.com>
2014-10-03 00:56:43 -0400

    fixed > io redirection
    < redirection however is completely broken

--- shell.c
+++ shell.c
@@ -13,6 +13,8 @@ written for CISC361 (Operating Systems)
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+int debug = 0;
+
 //exit string to compare tp
 char e[] = "exit";
 //path to bin
@@ -20,12 +22,14 @@ char bin[] = "/bin/";
 
 #define BUFFER_MAX_LENGTH 50
 static char buffer[BUFFER_MAX_LENGTH];
-int typedCount = 0;
+static char bufferFirstRun[BUFFER_MAX_LENGTH];
+static char bufferCut[BUFFER_MAX_LENGTH];
 static char userInput = '\0';
-static int bufferChars = 0;
-static char *commandArgv[5];
+static char *commandArgv[100];
 static int commandArgc = 0;
-char *inRedirect;
+char *redirectIn;
+char *redirectOut;
+char *redirect;
 
 /*
 A function to concatenate strings(should be in a header somewhere).
@@ -63,41 +67,54 @@ If the command is "exit" the program exits.
 */
 int checkBuiltInCommands()
 {
-    if(commandArgv[0] != NULL)
+    if(debug == 1)
     {
-
-    if(strcmp(e,commandArgv[0]) == 0)
-        {
-            exit(EXIT_SUCCESS);
-        }
+        printf("compare value: %d\n",strcmp("exit",commandArgv[0]));
     }
-        return 0;
+    if(strcmp(e,commandArgv[0]) == 0)
+    {
+        return -1;
+    }
+    return 0;
 }
 
 /*
 This function checks for the built in commands. If there weren't and built in
 commands fork and exec.
 */
-void handleUserCommands()
+void handleUserCommands(char** commandArgv)
 {
-    if(checkBuiltInCommands() == 0)
+    if(debug == 1)
+    {
+        printf("-commandArgV[0] right before handling is: %s\n",commandArgv[0]);
+    }
+    int check = checkBuiltInCommands();
+    if(debug == 1)
+    {
+    printf("user command was: %d\n",check);
+    }
+    if(check == 0)
     {
         pid_t pid = fork();
+        //int fileExists = access(concat(bin,commandArgv[0]), F_OK );
         if(pid == 0)
         {
-            if( access( concat(bin,commandArgv[0]), F_OK ) != -1 )
+            //strncpy(commandArgv[0],concat(bin,commandArgv[0]),strlen(commandArgv[0]));
+            if(debug == 1)
             {
-                printf("%d",access(concat(bin,commandArgv[0]),F_OK));
-                execvp(*commandArgv,commandArgv);
+            printf("your command is: %s\n",commandArgv[0]);
             }
-            else
-            {
-                exit(0);
-            }
+            execvp(commandArgv[0],commandArgv);
+            printf("%s : command not found*\n",commandArgv[0]);
+            exit(0);
         }
         else{
             wait(&pid);
         }
+    }
+    else if(check == -1)
+    {
+        exit(EXIT_SUCCESS);
     }
 }
 
@@ -111,57 +128,40 @@ void destroyCommand()
         commandArgv[commandArgc] = NULL;
         commandArgc--;
     }
-    bufferChars = 0;
 }
 
 /*
 Walks through a copy of the buffer and ads the values to the arguements array.
 */
-void populateCommand()
+char** populateCommand()
 {
-    char* bufferPointer;
+    if(debug == 1)
+    {
+        printf("-buffer right before populating command : %s\n",buffer);
+    }
+    char *bufferPointer;
     char buffercpy[BUFFER_MAX_LENGTH];
     strncpy(buffercpy,buffer,BUFFER_MAX_LENGTH);
-    bufferPointer = strtok(buffercpy," ");
+    if(debug == 1)
+    {
+        printf("-copy of buffer right before populating : %s\n",buffercpy);
+    }
+    bufferPointer = strtok(buffer," \n");
     while(bufferPointer != NULL)
         {
-        char toOut[strlen(bufferPointer)+1];
-        switch(bufferPointer[0])
-        {
-
-            case '>':
+            if(debug == 1)
             {
-                for(int i = 1;i<strlen(bufferPointer);i++)
-                    {
-                    toOut[i-1] = bufferPointer[i];
-                }
-                toOut[strlen(bufferPointer)-1] = '\0';
-                int out = open(toOut,O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-                dup2(out,1);
-                close(out);
+                printf("buffer tkn : %s\n",bufferPointer);
             }
-            break;
-            case '<':
-            {
-            }
-            break;
-            case '|':
-            {
-            }
-            break;
-            case '&':
-            {
-            }
-            break;
-            default:
-            {
                 commandArgv[commandArgc] = bufferPointer;
                 commandArgc++;
-            }
-            break;
+                bufferPointer = strtok(NULL," \n");
         }
-        bufferPointer = strtok(NULL," ");
-    }
+        if(debug == 1)
+        {
+            printf("end of populate : %s\n",commandArgv[0]);
+        }
+        return commandArgv;
 }
 
 /*
@@ -171,49 +171,120 @@ buffer which is reserved for the null terminator). It places what was in
 userInput into the buffer and at the end of its loop it ads a null terminator
 to the last element of the buffer.
 */
+
 void getTextLine()
 {
-    while((userInput != '\n') && (bufferChars < BUFFER_MAX_LENGTH))
-        {
-        buffer[bufferChars] = userInput;
-        typedCount++;
-        bufferChars++;
+    fgets(buffer,BUFFER_MAX_LENGTH,stdin);
+    strncpy(buffer,concat(&userInput,buffer),BUFFER_MAX_LENGTH);
+    if(debug == 1)
+    {
+    printf("-buffer: %s",buffer);
     }
-    buffer[bufferChars++] = 0x00;
 }
 
-/*
-This function runs through a copy of the buffer to check if any IO
-redirection (stdin only) needs to take place.
--return: 1 if there is a redirection in. 0 if no redirection is required.
-*/
-int checkForIOIn(){
-
-    char* bufferPointer;
-    char buffercpy[BUFFER_MAX_LENGTH];
-    strncpy(buffercpy,buffer,BUFFER_MAX_LENGTH);
-    bufferPointer = strtok(buffercpy," ");
-    while(bufferPointer != NULL){
-        switch(bufferPointer[0])
+int checkIORedirect(){
+    int numberOfRedirectsIn = 0;
+    int numberOfRedirectsOut = 0;
+    char *bufferPointer;
+    strncpy(bufferFirstRun,buffer,BUFFER_MAX_LENGTH);
+    bufferPointer = strtok(bufferFirstRun," \n");
+    while(bufferPointer != NULL)
         {
-            case '<':
+            switch(bufferPointer[0])
             {
-                inRedirect = bufferPointer;
-                return 1;
+                case '<':
+                {
+                    if(redirect == NULL)
+                    {
+                        redirect = bufferPointer;
+                    }
+                    redirectIn = bufferPointer;
+                    numberOfRedirectsIn++;
+                }
+                case '>':
+                {
+                    if(redirect == NULL)
+                    {
+                        redirect = bufferPointer;
+                    }
+                    redirectOut = bufferPointer;
+                    numberOfRedirectsOut++;
+                }
             }
-            break;
-            default:
-            {
-            }
-            break;
+        bufferPointer = strtok(NULL," \n");
         }
-        bufferPointer = strtok(NULL," ");
+    if(numberOfRedirectsIn > 1 || numberOfRedirectsOut > 1)
+    {
+        return -1;
     }
-    return 0;
+    else if(numberOfRedirectsIn == 1 || numberOfRedirectsOut == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-int length(const char* array[]) {
-  return sizeof(array)/sizeof(char*);
+int removeRestOfBuff()
+{
+    int found = 0;
+    for(int i = 0;buffer[i] != '\0' && found != 1;i++)
+    {
+        if(buffer[i] == '>' || buffer[i] == '<')
+        {
+            buffer[i] = '\0';
+            found = 1;
+            return i;
+        }
+    }
+    /*
+    char *bufferPointer;
+    int bufferSize = 0;
+    int *concatSize;
+    strncpy(bufferCut,buffer,BUFFER_MAX_LENGTH);
+    bufferPointer = strtok(bufferCut," \n");
+    while(bufferPointer != NULL)
+        {
+            switch(bufferPointer[0])
+            {
+                case '<':
+                {
+                    if(concatSize == NULL)
+                    {
+                        int currBuffSize = bufferSize;
+                        concatSize = &currBuffSize;
+                    }
+                }
+                case '>':
+                {
+                    if(concatSize == NULL)
+                    {
+                    int currBuffSize = bufferSize;
+                    concatSize = &currBuffSize;
+                    }
+                }
+                default:
+                {
+                    printf("sizeOfBuffer:%d\n",bufferSize);
+                    bufferSize += sizeof(bufferPointer)/sizeof(int);
+                }
+            }
+            bufferPointer = strtok(NULL," \n");
+        }
+        if(debug == 1)
+        {
+        //    printf("concatSize:%d\n",*concatSize);
+        }
+        //buffer[*concatSize] = '\0';
+        */
+    if(debug == 1)
+    {
+        printf("-buffer after cutting: %s\n",buffer);
+    }
+    return -1;
+
 }
 
 int main(){
@@ -226,7 +297,6 @@ int main(){
     sayPrompt();
     while(1){
         destroyCommand();
-        typedCount = 0;
         //get the first char
         userInput = getchar();
         switch(userInput)
@@ -237,39 +307,84 @@ int main(){
             break;
             default:
             {
-                //get what the user typed
                 getTextLine();
-                populateCommand();
-                //get through it once and look for IO redirection IN
-                if(checkForIOIn() == 1)
+                int redirects = checkIORedirect();
+                if(debug == 1)
                 {
-                    //open file redirect
-                    in = open(inRedirect,O_RDONLY,0);
-                    buffer[bufferChars] = ' ';
-                    getTextLine();
-                    dup2(in,0);
-                    buffer[bufferChars++] = ' ';
-                    getTextLine();
-                    dup2(stdinCpy,0);
-                    close(in);
+                    printf("-checked for redirect\n--redirectIn: %s\n--redirectOut: %s\n--redirect: %s\n",redirectIn,redirectOut,redirect);
                 }
-                destroyCommand();
-                populateCommand();
-                printf("buffer:%s",buffer);
-                for(int i = 0;i<length(commandArgv);i++ )
+                if(debug == 1)
                 {
-                printf("commands:%s",commandArgv[i]);
+                printf("-redirects: %d\n",redirects);
                 }
-                handleUserCommands();
-                dup2(stdoutCpy,1);
-                dup2(stdinCpy,0);
-                sayPrompt();
+                if(redirects == 1)
+                {
+                    if(redirectOut != NULL)
+                    {
+                        char toOut[strlen(redirectOut)+1];
+                        for(int i = 1;i<strlen(redirectOut);i++)
+                        {
+                            toOut[i-1] = redirectOut[i];
+                        }
+                        toOut[strlen(redirectOut)-1] = '\0';
+
+                        int out = open(toOut,O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+                        if(debug == 1)
+                        {
+                        }
+                        dup2(out,STDOUT_FILENO);
+                        close(out);
+                    }
+                    if(redirectIn != NULL)
+                    {
+                        char toIn[strlen(redirectIn)+1];
+                        for(int i = 1;i<strlen(redirectIn);i++)
+                            {
+                            toIn[i-1] = redirectIn[i];
+                        }
+                        toIn[strlen(redirectIn)-1] = '\0';
+                        int in = open(toIn,O_RDONLY);
+                        dup2(in,0);
+                        close(in);
+                        int start = removeRestOfBuff();
+                        char get[BUFFER_MAX_LENGTH];
+                        fgets(get,BUFFER_MAX_LENGTH,stdin);
+                        for(int i = 0;get[i] != '\n';i++)
+                        {
+                            buffer[start+i] = get[i];
+                        }
+                    }
+
+
+                }
+                else if(redirects == -1)
+                {
+                    if(debug == 1)
+                    {
+                    printf("-invalid redirecting\n");
+                    }
+                }
+                else
+                {
+                if(debug == 1)
+                {
+                printf("-no redirects found\n");
+                }
+                }
+
             }
             break;
         }
+        handleUserCommands(populateCommand());
+        dup2(stdoutCpy,STDOUT_FILENO);
+        dup2(stdinCpy,STDIN_FILENO);
+        sayPrompt();
+        redirectIn = NULL;
+        redirectOut = NULL;
+        redirect = NULL;
     }
-    //close(stdinCpy);
-    //close(stdoutCpy);
+    close(stdinCpy);
+    close(stdoutCpy);
     printf("\n");
     return(0);
 }
